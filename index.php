42f60a0b60153ee84ab63aeea10777fc6b38e45e
Derek Mauildin <derek.e.mauldin@gmail.com>
2016-04-27 17:21:46 -0600

    added code for database connection and code to display in a table adde css file

--- index.php
+++ index.php
@@ -1,10 +1,52 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Database Practice</title>
-  </head>
-  <body>
+<?php
 
-  </body>
-</html>
+/* php program to practice with databes handleing
+   written by Derek Mauldin
+   April 27, 2016
+*/
+
+require_once("derek.php");
+require_once("style.css");
+
+try {
+
+// setup dsn and options
+$dsn = 'mysql:host=' . $config["hostname"] . ';dbname=' . $config["database"];
+$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
+
+// set up pdo connection with database and set error attributes
+$pdo = new PDO($dsn, $config["username"], $config["password"], $options);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// setup query and execute
+$query = 'SELECT userId, username, userEmail, dateCreated FROM user';
+$statement = $pdo->prepare($query);
+$statement->execute();
+$statement->setFetchMode(PDO::FETCH_ASSOC);
+
+// setup table and table headers
+echo "<table>";
+echo "<tr>";
+echo "<th>User ID</th>";
+echo "<th>User Name</th>";
+echo "<th>User Email</th>";
+echo "<th>Date Created</th>";
+echo "</tr>";
+
+while(($row = $statement->fetch()) !== false) {
+  echo "<tr>";
+  echo ("<td>" . $row["userId"] . "</td>");
+  echo ("<td>" . $row["username"] . "</td>");
+  echo ("<td>" . $row["userEmail"] . "</td>");
+  echo ("<td>" . $row["dateCreated"] . "</td>");
+  echo "</tr>";
+}
+echo "</table>";  // end table
+
+$pdo = null;
+
+} catch (PDOException $e){
+    echo $e->getMessage();
+}
+
+ ?>
